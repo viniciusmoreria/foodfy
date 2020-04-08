699ba91761ec52efa6d0eb8da487e103552d4880
Vinícius Moreira <viniciusmoreeira@gmail.com>
2020-04-08 16:26:25 -0300

    refactoring folders

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -44,4 +44,54 @@ module.exports = {
       console.error(err);
     }
   },
+  async paginate(params) {
+    try {
+      const { filter, limit, offset } = params;
+
+      let query = "",
+        filterQuery = "",
+        totalQuery = `(SELECT count(*) FROM recipes) AS total`;
+
+      if (filter) {
+        filterQuery = `
+        WHERE recipes.title ILIKE '%${filter}%'
+        OR chefs.name ILIKE '%${filter}%'
+        `;
+
+        totalQuery = `(
+        SELECT count (*) FROM recipes
+        ${filterQuery}
+        ) as total`;
+      }
+
+      query = `
+          SELECT recipes.*, ${totalQuery}, chefs.name AS chef_name
+          FROM recipes
+          LEFT JOIN chefs ON(recipes.chef_id = chefs.id)
+          ${filterQuery}
+          ORDER BY updated_at DESC
+          LIMIT $1 OFFSET $2`;
+
+      const results = await db.query(query, [limit, offset]);
+      return results.rows;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  async files(id) {
+    try {
+      const results = await db.query(
+        `
+      SELECT files.*, recipe_files.recipe_id as recipe_id
+      FROM files 
+      LEFT JOIN recipe_files ON (files.id = recipe_files.file_id)
+      WHERE recipe_files.recipe_id = $1`,
+        [id]
+      );
+
+      return results.rows;
+    } catch (err) {
+      console.error(err);
+    }
+  },
 };
--- src/app/models/RecipeFile.js
+++ src/app/models/RecipeFile.js
@@ -3,5 +3,21 @@ const Base = require("./Base");
 Base.init({ table: "recipe_files" });
 
 module.exports = {
-  ...Base
+  ...Base,
+  async files(id) {
+    try {
+      const results = await db.query(
+        `
+      SELECT files.*, recipe_files.recipe_id as recipe_id
+      FROM files 
+      LEFT JOIN recipe_files ON (files.id = recipe_files.file_id)
+      WHERE recipe_files.recipe_id = $1`,
+        [id]
+      );
+
+      return results.rows;
+    } catch (err) {
+      console.error(err);
+    }
+  },
 };
